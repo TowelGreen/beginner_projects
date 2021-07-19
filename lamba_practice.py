1447008078d134e07dcf6c8e68c8a0fbb77e13f8
TowelGreen <84997975+TowelGreen@users.noreply.github.com>
2021-07-18 20:28:49 -0700

    Create lamba_practice.py

---
+++ lamba_practice.py
@@ -0,0 +1,12 @@
+
+fun1=lambda x,y:x * y
+
+fun2=lambda x: x+15
+
+func3=lambda x :isinstance(x,int)
+
+print("X time Y",fun1(10,3))
+
+print("X plus 15",fun2(10))
+
+print("Check if its a int",func3(7))
